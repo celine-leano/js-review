0dd43d32d4f599271cc3e5e61e2e63c165fb14c5
Celine Leano <cemleano@gmail.com>
2019-01-13 19:38:35 -0500

    Created FizzBuzz program

--- js-review.js
+++ js-review.js
@@ -5,4 +5,15 @@ Celine Leano
 JavaScript for js review assignment
  */
 
-console.log("Introduction to JavaScript");
+console.log("Introduction to JavaScript")
+
+// FizzBuzz program
+for (var i = 1; i <= 100; i++) {
+    if (i % 3 == 0) {
+        console.log("Fizz");
+    } else if (i % 5 == 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
